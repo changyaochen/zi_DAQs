95b0c0f37b7cfb796b06fcda0be14dbd5dd81906
changyaochen <changyao.chen@gmail.com>
2017-01-24 15:40:40 -0600

    Add files via upload
    make a new function in zi_processing.py: fit_lorentz_sweeper(df_in, showHTML = False, figure_name = 'temp')
    The purpose of this function is to fit the open loop data obtained from sweeper. The input should be in pd.DataFrame format
    Also appropriate changes of the vacmage_open_loop.py (oops, the name is wrong)... will need to correct it for next round

---
+++ vacmage_open_loop.py
@@ -0,0 +1,85 @@
+# -*- coding: utf-8 -*-
+"""
+This is for multiple open loop sweep with different vac
+The purpose is to see how the vac will affect the 
+lineshape of the resonance, as well as the 
+extracted Q
+
+@author: changyao chen
+"""
+
+from open_loop_sweep import *
+import os
+import numpy as np
+import pandas as pd
+import zi_processing
+
+# get the directories right
+dname = os.path.dirname(os.path.abspath(__file__))
+os.chdir(dname)
+device_id= 'dev267'
+
+
+# ===== global setting =======
+vac_list = np.linspace(0.01, 0.1, 20)
+inplace_fit = True  # whether to do Lorentz fit for each sweep
+# ==============================
+
+# I will save the output into 2 types of files:
+# for the first type, each vac has its own file
+# for the second type is the usual megasweep format (single file)
+# 
+# with vac attached to the file name
+
+type_II = pd.DataFrame()
+
+# only to extract certain fields from the raw output
+headers = ['frequency', 'x', 'y']
+
+# prompt to ask for path to save data
+handle = input('saved file path/name: ')
+if len(handle) == 0:
+    handle = 'temp'
+    
+# create a folder for the results
+if not os.path.exists(handle):
+    os.makedirs(handle)
+    os.chdir(dname + '/' + handle)
+elif handle == 'temp':
+    os.chdir(dname + '/' + handle)
+else:
+    raise Exception('Please input a valid file path/name!')
+        
+
+for vac in vac_list:
+    # run one open loop sweep
+    # the return result is a list, and its [0][0] element is the dict
+    # that is of interest
+    result = open_loop_sweep(device_id = 'dev267', 
+                             start_freq = 60.61e3, stop_freq = 60.65e3,
+                             amplitude=vac,
+                             samplecount = 2000)
+
+    type_I_temp = pd.DataFrame.from_dict({x: result[0][0][x] for x in headers})
+    type_I_temp['vac'] = vac
+               
+    # save the type I data
+    type_I_temp.to_csv(handle + '_' + str(vac) + '.txt', sep = '\t',
+                       index = False)
+    # do the fit if chosen
+    if inplace_fit:
+        zi_processing.fit_lorentz_sweeper(type_I_temp, showHTML = False, 
+                                          figure_name = handle + '_' + str(vac))
+    
+    if type_II.size == 0:  # first time
+        type_II = type_I_temp
+    else:
+        type_II = type_II.append(type_I_temp, ignore_index = True)
+        
+# save the type_II data
+type_II.to_csv(handle + '_vacmega.txt', sep = '\t', 
+               index = False, headers = False)
+        
+
+# return to the parent folder
+os.chdir('..')
--- zi_processing.py
+++ zi_processing.py
@@ -11,6 +11,17 @@ import scipy as sp
 import pandas as pd
 import bokeh.plotting as bkp
 
+def lorentz(p, x):
+      A, f0, Q, bkg = p
+      return A / np.sqrt((x**2 - f0**2)**2 + (x * f0 / Q)**2) + bkg
+      # p[0] is Amplitude
+      # p[1] is resonant frequency
+      # p[2] is Q
+      # p[3] is background
+    
+def errorfunc(p, x, z):
+      return lorentz(p,x) - z
+
 def open_loop_zi(folder, fit_lorentz = False, showHTML = False):
   """
   This is to process the data obtained from sweeper 
@@ -41,16 +52,6 @@ def open_loop_zi(folder, fit_lorentz = False, showHTML = False):
   
   if fit_lorentz:
     # fitting!
-    def lorentz(p, x):
-      A, f0, Q, bkg = p
-      return A / np.sqrt((x**2 - f0**2)**2 + (x * f0 / Q)**2) + bkg
-      # p[0] is Amplitude
-      # p[1] is resonant frequency
-      # p[2] is Q
-      # p[3] is background
-    
-    def errorfunc(p, x, z):
-          return lorentz(p,x) - z
         
     Q_guess = 200000
     Amp = raw_data['R(Vrms)']
@@ -84,6 +85,58 @@ def open_loop_zi(folder, fit_lorentz = False, showHTML = False):
     
   # TODO: get the error bound
   
-  
+def fit_lorentz_sweeper(df_in, showHTML = False, figure_name = 'temp'):
+    """
+    This is to fit the data obtained by sweeper
+    The input is a dataframe, which needs to have
+    'frequency', 'x', 'y' columns
+    
+    return fitted parameter, <list> solp
+    [A, f0, Q, bkg]
+   
+    """
+    # make a copy!
+    df = df_in.copy()
+    
+    assert isinstance(df, pd.DataFrame), 'Input type must be pd.DataFrame!'
+    columns = ['x', 'y', 'frequency']
+    for elem in columns:
+        assert elem in df.columns, 'No %s column!' % elem
+    
+    Q_guess = 200000    
+    Amp = np.sqrt(df['x']**2 + df['y']**2)
+    df['r'] = Amp
+    f0_guess = df['frequency'].ix[np.argmax(Amp)]
+    bkg_guess = 0.00001
+    p_guess = [max(Amp) * Q_guess, f0_guess, Q_guess, bkg_guess]
+    solp, pcov, infodict, errmsg, success\
+    = sp.optimize.leastsq(errorfunc, 
+                    p_guess, 
+                    args=(df['frequency'], Amp),
+                    Dfun=None,
+                    full_output=True,
+                    ftol=1e-9,
+                    xtol=1e-9,
+                    maxfev=100000,
+                    epsfcn=1e-10,
+                    factor=0.1)
+    
+    bkp.output_file(figure_name + '.html')
+    # start plotting!
+    p1 = bkp.figure(plot_width=800, plot_height=300, title = figure_name, 
+                   tools='pan,box_zoom,reset,resize,save')
+    p1_source = bkp.ColumnDataSource(df)
+    p1.circle(x = 'frequency', y = 'r', source = p1_source, size = 10, 
+            alpha = 0.5, legend = 'Data')
+
+    p1.xaxis[0].axis_label = 'Frequency (Hz)'
+    p1.yaxis[0].axis_label = 'Voltage (V)'
+    if showHTML:
+        bkp.show(p1)
+    else:
+        bkp.save(p1)
+    
+    return solp
+    
   
   
