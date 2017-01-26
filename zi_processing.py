2be8e161749bd7c56ce626708440bf399094ba48
changyaochen <changyao.chen@gmail.com>
2017-01-26 15:45:23 -0600

    two changes: 1) working on the fit_lorentz_sweeper(), such that it will work for the data taken directly taken from the zi app (sweeper tag) 2) for the peak fitting, focusing on the range of few (30 for now) line widths around the f0

--- zi_processing.py
+++ zi_processing.py
@@ -83,7 +83,8 @@ def open_loop_zi(folder, fit_lorentz = False, showHTML = False):
     bkp.save(p1)
     
   
-def fit_lorentz_sweeper(df_in, showHTML = False, figure_name = 'temp'):
+def fit_lorentz_sweeper(df_in, showHTML = False, 
+                        figure_name = 'temp', native_zi = False):
     """
     This is to fit the data obtained by sweeper
     The input is a dataframe, which needs to have
@@ -91,26 +92,47 @@ def fit_lorentz_sweeper(df_in, showHTML = False, figure_name = 'temp'):
     
     return fitted parameter, <list> solp
     [A, f0, Q, bkg]
+    
+    if native_zi == True, which meaning the data is taken
+    directly from zi's web-based sweeper tag
+    then the columns will be
+    'F(Hz)', and 'R(Vrms)'
    
     """
     # make a copy!
     df = df_in.copy()
     
     assert isinstance(df, pd.DataFrame), 'Input type must be pd.DataFrame!'
-    columns = ['x', 'y', 'frequency']
-    for elem in columns:
+    if native_zi:
+      columns = ['F(Hz)', 'R(Vrms)']
+      for elem in columns:
         assert elem in df.columns, 'No %s column!' % elem
+      freq_header, r_header = 'F(Hz)', 'R(Vrms)'
+    else:
+      columns = ['x', 'y', 'frequency']
+      for elem in columns:
+        assert elem in df.columns, 'No %s column!' % elem
+      Amp = np.sqrt(df['x']**2 + df['y']**2)
+      df['r'] = Amp
+      freq_header, r_header = 'frequency', 'r'
+    
+        
     
     Q_guess = 200000    
-    Amp = np.sqrt(df['x']**2 + df['y']**2)
-    df['r'] = Amp
-    f0_guess = df['frequency'].ix[np.argmax(Amp)]
+    Amp = df[r_header]
+    f0_guess = df[freq_header].ix[np.argmax(Amp)]
+    # let's just focus on the section arounding this area
+    f_bound = 15*f0_guess / Q_guess
+    mask = (df[freq_header] < f0_guess + f_bound) \
+           & ((df[freq_header] > f0_guess - f_bound))
+#    print(sum(mask))
+    df2 = df[mask]
     bkg_guess = 0.00001
     p_guess = [max(Amp) * Q_guess, f0_guess, Q_guess, bkg_guess]
     p_fit, pcov, infodict, errmsg, success\
     = sp.optimize.leastsq(errorfunc, # errorfunc (p, x, y_data)
                     p_guess, 
-                    args=(df['frequency'], Amp),
+                    args=(df2[freq_header], df2[r_header]),
                     Dfun=None,
                     full_output=True,
                     ftol=1e-9,
@@ -119,19 +141,22 @@ def fit_lorentz_sweeper(df_in, showHTML = False, figure_name = 'temp'):
                     epsfcn=1e-10,
                     factor=0.1)
     
-    # TODO: ========== get the error bound ==========
+    # ========== get the error bound ==========
     # following the info from this link:
     # http://stackoverflow.com/questions/14581358/getting-standard-errors-on-fitted-parameters-using-the-optimize-leastsq-method-i
     # I will also assume the  # of data points is larger than the # of fitting
     # parameters
     
     # first calculate the residual variance s_sq
-    s_sq = (errorfunc(p_fit, df['frequency'], Amp)**2).sum()\
-                      /(len(Amp) - len(p_fit))
-#    print('residual variance is: ', s_sq)
+    if pcov is not None:
+      s_sq = (errorfunc(p_fit, df2[freq_header], df2[r_header])**2).sum()\
+                      /(len(df2[r_header]) - len(p_fit))
+#      print('residual variance is: ', s_sq)
 
-    # update the pcov
-    pcov = pcov * s_sq
+      # update the pcov
+      pcov = pcov * s_sq
+    else:
+      pcov = np.inf
     
     # calculate the sqrt of the diagonal component of pcov
     p_err = []
@@ -151,9 +176,9 @@ def fit_lorentz_sweeper(df_in, showHTML = False, figure_name = 'temp'):
     p1 = bkp.figure(plot_width=800, plot_height=300, title = figure_name, 
                    tools='pan,box_zoom,reset,resize,save')
     p1_source = bkp.ColumnDataSource(df)
-    p1.circle(x = 'frequency', y = 'r', source = p1_source, size = 10, 
+    p1.circle(x = freq_header, y = r_header, source = p1_source, size = 10, 
             alpha = 0.5, legend = 'Data')
-    p1.line(df['frequency'], lorentz(p_fit, df['frequency']), color = 'red', 
+    p1.line(df2[freq_header], lorentz(p_fit, df2[freq_header]), color = 'red', 
                    line_dash = 'dashed', line_width = 3, 
                    legend = 'Fitted, Q = ' + str(round(p_fit[2],3)))
 
